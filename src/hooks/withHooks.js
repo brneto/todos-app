2a9ecf80d3de44cd3b08cf4dc7eb19fe1947ddc9
Berchris Requiao <brneto@gmail.com>
2018-12-13 14:17:04 -0300

    Change hooks to simple example

--- src/hooks/withHooks.js
+++ src/hooks/withHooks.js
@@ -1,34 +1,23 @@
 import React, { useState, useEffect } from 'react';
 
-const useMedia = (query) => {
-  let [matches, setMatches] = useState(
-    window.matchMedia(query).matches
-  );
+function useWindowsWidth() {
+  const [width, setWidth] = useState(window.innerWidth);
+
+  const handleResize = () => setWidth(window.innerWidth);
 
   useEffect(() => {
-    let media = window.matchMedia(query);
-    if(media.matches !== matches) {
-      setMatches(media.matches);
-    }
+    window.addEventListener('resize', handleResize);
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
   });
 
-  return matches;
-};
+  return width;
+}
 
-const App = () => {
-  let small = useMedia('(max-width: 400px)');
-  let large = useMedia('(min-width: 800px)');
-  return (
-    <div className="Media">
-      <h1>Media</h1>
-      <p>
-        Small? {small ? 'Yep' : 'Nope'}.
-      </p>
-      <p>
-        Large? {large ? 'Yep' : 'Nope'}.
-      </p>
-    </div>
-  );
-};
+function MyResponsiveComponent() {
+  const width = useWindowsWidth(); // My custom Hook
+  return (<p>Window width is {width}.</p>);
+}
 
-export default App;
+export default MyResponsiveComponent;
