5e143ecd6621155d075ab69be9df68e26fe7e6f2
Berchris Requiao <brneto@gmail.com>
2018-12-13 22:49:42 -0300

    Convert AddTodo to use hooks

--- src/client/components/AddTodo.js
+++ src/client/components/AddTodo.js
@@ -1,48 +1,54 @@
-import { Component } from 'react';
-import { bindActionCreators } from 'redux';
-import { produce } from 'immer';
+import React from 'react';
 import PropTypes from 'prop-types';
+import styled from 'styled-components';
 import { connect } from 'react-redux';
-import { addTodo } from '../redux/actions';
+import { useAddTodo } from '../hooks/todo';
 
-@connect()
-class AddTodo extends Component {
-  static propTypes = {
-    render: PropTypes.func.isRequired,
-    dispatch: PropTypes.func.isRequired
-  };
+const Title = styled.h1`
+  position: absolute;
+  top: -155px;
+  width: 100%;
+  color: rgba(175, 47, 47, .15);
+  font-size: 100px;
+  font-weight: 100;
+  text-align: center;
+  text-rendering: optimizeLegibility;
+`;
 
-  state = { todo: '' };
-  addTodo = bindActionCreators(addTodo, this.props.dispatch);
+const Input = styled.input`
+  position: relative;
+  box-sizing: border-box;
+  width: 100%;
+  margin: 0;
+  padding: 16px;
+  border: none;
+  background: rgba(0, 0, 0, .003);
+  color: inherit;
+  font-family: inherit;
+  font-size: 24px;
+  font-weight: inherit;
+  line-height: 1.4em;
+  box-shadow: inset 0 -2px 1px rgba(0, 0, 0, .03);
 
-  handleChange = ({ target }) =>
-    this.setState(produce(draft => {
-      draft.todo = target.value;
-    }));
-
-  handleKeyDown = ({ keyCode }) => {
-    if (keyCode === 13) { //ENTER_KEY
-      this.setState(produce(draft => {
-        this.addTodo(draft.todo.trim());
-        draft.todo = '';
-      }));
-    }
-  };
-
-  render() {
-    const formProps = {
-      input: {
-        value: this.state.todo,
-        onChange: this.handleChange,
-        onKeyDown: this.handleKeyDown
-      }
-    };
-
-    // const { render: Comp } = this.props;
-    // return <Comp {...formProps} />;
-    const { render } = this.props;
-    return render(formProps);
+  ::placeholder {
+    color: #e6e6e6;
+    font-style: italic;
+    font-weight: 300;
   }
+`;
+
+export function AddTodoPresentation({ dispatch }) {
+  const input = useAddTodo(dispatch);
+  return (
+    <header>
+      <Title>todos</Title>
+      <Input name="todo" placeholder="What needs to be done?" {...input} />
+    </header>
+  );
 }
 
-export default AddTodo;
+AddTodoPresentation.propTypes = {
+  dispatch: PropTypes.func.isRequired,
+};
+
+export default connect()(AddTodoPresentation);
