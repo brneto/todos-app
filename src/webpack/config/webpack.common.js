e2ab3e0aed47fae0917b579520b774c8c7e640f9
Berchris Requiao <berchris@mpdft.mp.br>
2019-01-10 13:09:13 -0300

    Add babel optional catch binding

--- src/webpack/config/webpack.common.js
+++ src/webpack/config/webpack.common.js
@@ -95,6 +95,7 @@ const commonConfig = {
                 '@babel/plugin-proposal-function-bind',
                 '@babel/plugin-proposal-optional-chaining',
                 '@babel/plugin-proposal-throw-expressions',
+                '@babel/plugin-proposal-optional-catch-binding',
                 // It's required Babel Syntax Dynamic Import Plugin to
                 // Webpack Dynamic Imports work.
                 '@babel/plugin-syntax-dynamic-import',
