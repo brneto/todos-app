12fe465932344f99e10fb63bac64bc501416cf1f
Berchris Requiao <berchris@mpdft.mp.br>
2019-01-10 13:10:20 -0300

    Change order

--- src/webpack/config/webpack.common.js
+++ src/webpack/config/webpack.common.js
@@ -94,8 +94,8 @@ const commonConfig = {
                 ['@babel/plugin-proposal-class-properties', { loose: true }],
                 '@babel/plugin-proposal-function-bind',
                 '@babel/plugin-proposal-optional-chaining',
-                '@babel/plugin-proposal-optional-catch-binding',
                 '@babel/plugin-proposal-throw-expressions',
+                '@babel/plugin-proposal-optional-catch-binding',
                 // It's required Babel Syntax Dynamic Import Plugin to
                 // Webpack Dynamic Imports work.
                 '@babel/plugin-syntax-dynamic-import',
