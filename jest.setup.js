a8354a1f0b40e31f1de92eddd7bcdf7d3341a42d
Berchris Requiao <brneto@gicloud.com>
2019-03-24 02:12:33 +0000

    Update babel configuration

--- jest.setup.js
+++ jest.setup.js
@@ -1,7 +1,7 @@
 //https://babeljs.io/docs/en/next/v7-migration#remove-proposal-polyfills-in-babel-polyfill-https-githubcom-babel-babel-issues-8416
-//https://github.com/babel/babel/commit/c8bb4500326700e7dc68ce8c4b90b6482c48d82f
-import 'core-js/fn/array/flat-map';
-import 'core-js/fn/string/match-all';
+//https://babeljs.io/blog/2019/03/19/7.4.0
+//https://github.com/babel/babel/blob/master/packages/babel-polyfill/src/index.js
+import 'core-js/modules/esnext.string.match-all';
 
 // add some helpful assertions
 import 'jest-dom/extend-expect';
