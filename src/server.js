9c335d57a9d6ab4e4a33ccc95de7241df77f392b
Berchris Requiao <brneto@gicloud.com>
2019-03-26 01:35:50 +0000

    Fix jest using watchman and add corejs version to webpack

--- src/server.js
+++ src/server.js
@@ -1,23 +1,22 @@
 //import '@babel/polyfill';
+import http from 'http';
 import chalk from 'chalk';
-import compression from 'compression';
-import express from 'express';
 import morgan from 'morgan';
+import compression from 'compression';
 import open from 'open';
+import express from 'express';
 import webpackBuilder from './webpack';
 
-const app = express();
 const port = 3000;
+const app = express();
+const server = http.createServer(app);
 
 // eslint-disable-next-line no-console
 console.log(chalk.green(`Starting app in ${process.env.NODE_ENV} mode...`));
 
-app.use(morgan('combined'));
-app.use(compression());
 webpackBuilder().then(middleware => {
-  app.use(middleware);
-  app.listen(
-    port,
+  app.use(morgan('combined'), compression(), middleware);
+  server.listen(port,
     // eslint-disable-next-line no-console
     err => (err ? console.log(err) : open(`http://localhost:${port}`))
   );
