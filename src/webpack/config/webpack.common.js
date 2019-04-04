66f113f1174dc645b150700954caedaa2e0389b1
Berchris Requiao <35544241+brneto@users.noreply.github.com>
2019-04-04 16:49:13 +0100

    Remove already included proposal in preset-env

--- src/webpack/config/webpack.common.js
+++ src/webpack/config/webpack.common.js
@@ -93,11 +93,9 @@ const commonConfig = {
                 'react-hot-loader/babel',
                 ['@babel/plugin-proposal-decorators', { legacy: true }],
                 ['@babel/plugin-proposal-class-properties', { loose: true }],
-                '@babel/plugin-proposal-object-rest-spread',
                 '@babel/plugin-proposal-function-bind',
                 '@babel/plugin-proposal-optional-chaining',
                 '@babel/plugin-proposal-throw-expressions',
-                '@babel/plugin-proposal-optional-catch-binding',
                 // It's required Babel Syntax Dynamic Import Plugin to
                 // Webpack Dynamic Imports work.
                 '@babel/plugin-syntax-dynamic-import',
