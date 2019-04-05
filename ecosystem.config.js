37b45083b5edf989c3aaa77ecd170380a617e2d3
Berchris Requiao <berchris.requiao@neueda.com>
2019-04-05 01:51:57 +0100

    Move server code to specific folder

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,10 +2,10 @@ module.exports = {
   // Options reference: https://pm2.io/doc/en/runtime/reference/ecosystem-file/
   apps : [{
     name: 'app',
-    script: 'src/server.js',
+    script: 'src/server/index.js',
     interpreter: 'node_modules/.bin/babel-node',
     autorestart: false,
-    watch: ['src/server.js', 'src/builder'],
+    watch: ['src/server', 'src/webpack', 'src/builder'],
     ignore_watch: '*.test.js',
     output: 'logs/app-out.log',
     error: 'logs/app-err.err',
