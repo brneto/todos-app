e9473bfd118b28eebe80bc6e4e18bf3867f38a72
Berchris Requiao <35544241+brneto@users.noreply.github.com>
2019-04-05 20:15:41 +0100

    Update webpack.common.js

--- src/webpack/config/webpack.common.js
+++ src/webpack/config/webpack.common.js
@@ -94,6 +94,7 @@ const commonConfig = {
                 ['@babel/plugin-proposal-class-properties', { loose: true }],
                 '@babel/plugin-proposal-function-bind',
                 '@babel/plugin-proposal-optional-chaining',
+                '@babel/plugin-proposal-private-methods',
                 '@babel/plugin-proposal-throw-expressions',
                 // It's required Babel Syntax Dynamic Import Plugin to
                 // Webpack Dynamic Imports work.
