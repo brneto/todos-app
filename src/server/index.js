2099afe9124142f02248e9c377da4e87f8511a50
Berchris Requiao <berchris.requiao@neueda.com>
2019-04-06 03:07:39 +0100

    Add server hmr template

--- src/server/index.js
+++ src/server/index.js
@@ -14,6 +14,21 @@ console.log(chalk.green(`Starting app in ${process.env.NODE_ENV} mode...`));
 
 webpackBuilder().then(middleware => {
   app.use(morgan('combined'), compression(), ...middleware);
+
+  // Do "hot-reloading" of express stuff on the server
+  // Throw away cached modules and re-require next time
+  // Ensure there's no important state in there!
+  // const watcher = chokidar.watch('./server');
+
+  // watcher.on('ready', function() {
+  //   watcher.on('all', function() {
+  //     console.log("Clearing /server/ module cache from server");
+  //     Object.keys(require.cache).forEach(function(id) {
+  //       if (/[\/\\]server[\/\\]/.test(id)) delete require.cache[id];
+  //     });
+  //   });
+  // });
+
   app.listen(port, error =>
     error
       ? console.log(`Server failed to start: [${error}].`)
---
+++ src/server/templateIndex.js
@@ -0,0 +1,61 @@
+import chokidar from 'chokidar';
+import config from './webpack.config';
+import cssModulesRequireHook from 'css-modules-require-hook';
+import express from 'express';
+import http from 'http';
+import webpack from 'webpack';
+import webpackDevMiddleware from 'webpack-dev-middleware';
+import webpackHotMiddleware from 'webpack-hot-middleware';
+
+cssModulesRequireHook({generateScopedName: '[path][name]-[local]'});
+const compiler = webpack(config);
+const app = express();
+
+// Serve hot-reloading bundle to client
+app.use(webpackDevMiddleware(compiler, {
+  noInfo: true, publicPath: config.output.publicPath
+}));
+app.use(webpackHotMiddleware(compiler));
+
+// Include server routes as a middleware
+app.use(require('./server/app'));
+
+// Do "hot-reloading" of express stuff on the server
+// Throw away cached modules and re-require next time
+// Ensure there's no important state in there!
+const watcher = chokidar.watch('./server');
+
+watcher.on('ready', function() {
+  watcher.on('all', function() {
+    console.log("Clearing /server/ module cache from server");
+    Object.keys(require.cache).forEach(function(id) {
+      if (/[\/\\]server[\/\\]/.test(id)) delete require.cache[id];
+    });
+  });
+});
+
+// Anything else gets passed to the client app's server rendering
+app.get('*', function(req, res, next) {
+  require('./client/server-render')(req.path, function(err, page) {
+    if (err) return next(err);
+    res.send(page);
+  });
+});
+
+// Do "hot-reloading" of react stuff on the server
+// Throw away the cached client modules and let them be re-required next time
+compiler.plugin('done', function() {
+  console.log("Clearing /client/ module cache from server");
+  Object.keys(require.cache).forEach(function(id) {
+    if (/[\/\\]client[\/\\]/.test(id)) delete require.cache[id];
+  });
+});
+
+const server = http.createServer(app);
+server.listen(3000, 'localhost', function(err) {
+  if (err) throw err;
+
+  const addr = server.address();
+
+  console.log('Listening at http://%s:%d', addr.address, addr.port);
+});
