2e1e2c44ffbf17c099b07a9e0670e261234aa627
Berchris Requiao <35544241+brneto@users.noreply.github.com>
2019-04-06 01:37:03 +0100

    Update build.util.js

--- src/webpack/builder/build.util.js
+++ src/webpack/builder/build.util.js
@@ -7,7 +7,7 @@ export const getResourcePath = (cfg, rsc) => path.join(cfg.output.path, rsc);
 export const createResourceBuffer = (cfg, fs) => rsc =>
   fs.readFileSync(getResourcePath(cfg, rsc));
 
-export const spaServerRules = (indexResourceName, getResource) => (req, res, next) => {
+export const spaServerRules = (getResource, indexResourceName) => (req, res, next) => {
   const requestedResource = getRequestedResource(req);
   const indexResource = getResource(indexResourceName);
 
