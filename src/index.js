4d9de1c6c7af8129905c1a4ca8b9f77a6d54e943
Berchris Requiao <brneto@gicloud.com>
2019-04-12 21:36:54 +0100

    Add todo comment

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 //import '@babel/polyfill';
-import chokidar from 'chokidar';
 import chalk from 'chalk';
 import morgan from 'morgan';
 import compression from 'compression';
@@ -30,27 +29,14 @@ if (env === 'production') {
   app.use(morgan('tiny'), compression(), express.static(config.path));
   app.listen(port, listenerChecker);
 } else if (env === 'building') {
+    // TODO: Change to use webpack to also transpile the server code.
+    // https://github.com/yusinto/universal-hot-reload
+    // https://github.com/liady/webpack-node-externals
   import('./webpack/builder/build.prod');
 } else {
   import('./webpack/builder/build.dev').then(async module => {
     const middlewares = await module.default;
     app.use(morgan('combined'), compression(), ...middlewares);
-
-    //https://github.com/glenjamin/ultimate-hot-reloading-example/blob/master/server.js
-    // TODO: Test whether the server hot-reloading it's really working.
-    // Do "hot-reloading" of express stuff on the server
-    // Throw away cached modules and re-require next time
-    // Ensure there's no important state in there!
-    const watcher = chokidar.watch('./src/server');
-    watcher.on('ready', () => {
-      watcher.on('all', () => {
-        Object.keys(require.cache).forEach(
-          id => /[\\/]server[\\/]/.test(id) && delete require.cache[id]
-        );
-        console.log(chalk.yellow('"server" module cache cleared'));
-      });
-    });
-
     app.listen(port, listenerChecker);
   });
 }
