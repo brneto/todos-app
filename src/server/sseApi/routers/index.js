ae8f4678760e18597e07966e9c52f520f1497b83
Berchris Requiao <berchris.requiao@ericsson.com>
2019-04-17 10:12:35 +0100

    Update modules

--- src/server/sseApi/routers/index.js
+++ src/server/sseApi/routers/index.js
@@ -1,34 +1,5 @@
-// https://jasonbutz.info/2018/08/server-sent-events-with-node/
-import { Router } from 'express';
-
-const router = Router(); // eslint-disable-line babel/new-cap
-
-let sendEvent;
-const createEventSender = res => msg => {
-  let id = 0;
-
-  res.write(`id: ${++id}\n`);
-  res.write('event: messages\n');
-  res.write(`data: ${msg} -- timestamp: ${Date.now()}\n\n`);
-};
-
-router.get('/sse', (req, res) => {
-  res.writeHead(200, {
-    'Content-Type': 'text/event-stream',
-    'charset': 'utf-8',
-    'Cache-Control': 'no-cache',
-    'Connection': 'keep-alive',
-  });
-  res.write('\n');
-
-  sendEvent = createEventSender(res);
-});
-
-router.post('/sse/:msg', (req, res) => {
-  sendEvent(req.params.msg);
-  res.send('Server Sent Event sended successful.\n');
-});
+import sse from './sse';
 
 export default {
-  sse: router,
+  sse,
 };
---
+++ src/server/sseApi/routers/sse.js
@@ -0,0 +1,32 @@
+// https://jasonbutz.info/2018/08/server-sent-events-with-node/
+import { Router } from 'express';
+
+const router = Router(); // eslint-disable-line babel/new-cap
+
+let sendEvent;
+const createEventSender = res => msg => {
+  let id = 0;
+
+  res.write(`id: ${++id}\n`);
+  res.write('event: messages\n');
+  res.write(`data: ${msg} -- timestamp: ${Date.now()}\n\n`);
+};
+
+router.get('/sse', (req, res) => {
+  res.writeHead(200, {
+    'Content-Type': 'text/event-stream',
+    'charset': 'utf-8',
+    'Cache-Control': 'no-cache',
+    'Connection': 'keep-alive',
+  });
+  res.write('\n');
+
+  sendEvent = createEventSender(res);
+});
+
+router.post('/sse/:msg', (req, res) => {
+  sendEvent(req.params.msg);
+  res.send('Server-sent event sended successful.\n');
+});
+
+export default router;
