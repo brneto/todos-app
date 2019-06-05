fbc2da6dc0ff7e56d56da96f7199a3a2d83320d6
Berchris Requiao <35544241+brneto@users.noreply.github.com>
2019-06-05 11:42:59 +0100

    Update webpack.prod.js

--- src/webpack/configs/webpack.prod.js
+++ src/webpack/configs/webpack.prod.js
@@ -3,7 +3,7 @@ import webpack from 'webpack';
 import merge from 'webpack-merge';
 import TerserPlugin from 'terser-webpack-plugin';
 import OptimizeCSSAssetsPlugin from 'optimize-css-assets-webpack-plugin';
-import CleanWebpackPlugin from 'clean-webpack-plugin';
+import { CleanWebpackPlugin } from 'clean-webpack-plugin';
 import HtmlWebpackPlugin from 'html-webpack-plugin';
 import ManifestPlugin from 'webpack-manifest-plugin';
 import WorkboxPlugin from 'workbox-webpack-plugin';
