72b3a2cecb7cd342364af67d8cfad9b83b7d5a23
Berchris Requiao <35544241+brneto@users.noreply.github.com>
2019-08-13 00:33:43 +0100

    Update es9Arrays.spec.js

--- src/__tests__/es9/es9Arrays.spec.js
+++ src/__tests__/es9/es9Arrays.spec.js
@@ -1,5 +1,4 @@
-describe('Array', () => {
-  describe('With inside arrays', () => {
+describe('Inner arrays', () => {
     const sourceArray = [
       {
         documento: 'PGR-1',
@@ -59,21 +58,20 @@ describe('Array', () => {
       const flattedArray = sourceArray.flatMap(e => e.arquivos);
       expect(flattedArray).toEqual(expectArray);
     });
+});
+
+describe('Arrays', () => {
+  const primes = [2, 3, 5, 7, 11];
+  const [first, second, ...rest] = primes;
+
+  it('should support destructuring assigment', () => {
+    expect(first).toEqual(2);
+    expect(second).toEqual(3);
+    expect(rest).toEqual([5, 7, 11]);
   });
 
-  describe('Rest elements', () => {
-    const primes = [2, 3, 5, 7, 11];
-    const [first, second, ...rest] = primes;
-
-    it('should support destructuring assigment', () => {
-      expect(first).toEqual(2);
-      expect(second).toEqual(3);
-      expect(rest).toEqual([5, 7, 11]);
-    });
-
-    it('should support spread elements', () => {
-      const primesCopy = [first, second, ...rest];
-      expect(primesCopy).toEqual([2, 3, 5, 7, 11]);
-    });
+  it('should support spread elements', () => {
+    const primesCopy = [first, second, ...rest];
+    expect(primesCopy).toEqual([2, 3, 5, 7, 11]);
   });
 });
