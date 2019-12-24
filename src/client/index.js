b056efe8c10606cb405a62971a5927cd379c4168
Berchris Requiao <berchris.requiao@neueda.com>
2019-12-24 12:19:53 +0000

    Update client index component

--- src/client/index.js
+++ src/client/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import { render } from 'react-dom';
+import ReactDOM from 'react-dom';
 import Root from './components/Root';
 import * as state from './redux/store';
 import * as serviceWorker from './serviceWorker';
 
-render(
+ReactDOM.render(
   <Root {...state} />,
   document.getElementById('root')
 );
