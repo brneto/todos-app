66c7695d6de82433dbf5668d061b77950cccc041
Berchris Requiao <berchris.requiao@ericsson.com>
2020-01-02 09:27:51 +0000

    Remove no needed import in test config

--- tests.setup.js
+++ tests.setup.js
@@ -1,10 +1,8 @@
-//https://babeljs.io/docs/en/next/v7-migration#remove-proposal-polyfills-in-babel-polyfill-https-githubcom-babel-babel-issues-8416
-//https://babeljs.io/blog/2019/03/19/7.4.0
-//https://github.com/babel/babel/blob/master/packages/babel-polyfill/src/index.js
-import 'core-js/modules/esnext.string.match-all';
+// https://babeljs.io/blog/2019/03/19/7.4.0
+// https://babeljs.io/docs/en/next/v7-migration#remove-proposal-polyfills-in-babel-polyfill-https-githubcom-babel-babel-issues-8416
 
 // add some helpful assertions
 import '@testing-library/jest-dom/extend-expect';
 
-//https://stackoverflow.com/questions/42677387/jest-returns-network-error-when-doing-an-authenticated-request-with-axios/43020260#43020260
-//global.XMLHttpRequest = undefined;
+// https://stackoverflow.com/questions/42677387/jest-returns-network-error-when-doing-an-authenticated-request-with-axios/43020260#43020260
+// global.XMLHttpRequest = undefined;
