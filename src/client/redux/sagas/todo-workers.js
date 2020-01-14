d72f84dc7d3121b5a96e6e290bbf9de5c0079594
Berchris Requiao <35544241+brneto@users.noreply.github.com>
2020-01-14 01:19:52 +0000

    Update todo-workers.js

--- src/client/redux/sagas/todo-workers.js
+++ src/client/redux/sagas/todo-workers.js
@@ -2,6 +2,6 @@ import { featureIsEnabled } from '../../../feature-router';
 import * as workersPristine from './todo-workers.pristine';
 import * as workersFeatured from './todo-workers.featured';
 
-export const
-  { fetchTodos, addTodo, toggleTodo } = workersPristine,
-  getTodosResource = featureIsEnabled('use-new-React-Suspense') ? workersFeatured : undefined;
+export const {
+  getTodosResource, fetchTodos, addTodo, toggleTodo
+} = featureIsEnabled('use-new-React-Suspense') ? workersFeatured : workersPristine;
