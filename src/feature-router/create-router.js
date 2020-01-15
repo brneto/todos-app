d6c864a4bed13966be35c4e94fb90a5ae34efbdb
Berchris Requiao <35544241+brneto@users.noreply.github.com>
2020-01-15 17:07:13 +0000

    Update create-router.js

--- src/feature-router/create-router.js
+++ src/feature-router/create-router.js
@@ -1,4 +1,4 @@
 export default featureConfig => ({
-  setFeature: (featureName, isEnabled) => void (featureConfig[featureName] = isEnabled),
+  setFeature: (featureName, isEnabled) => { featureConfig[featureName] = isEnabled; },
   featureIsEnabled: featureName => featureConfig[featureName],
 });
