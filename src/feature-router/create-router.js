598ed054e1248f25ffcb3118b471c1172e950364
Berchris Requiao <berchris.requiao@neueda.com>
2020-01-23 23:07:31 +0000

    Update commas

--- src/feature-router/create-router.js
+++ src/feature-router/create-router.js
@@ -1,6 +1,6 @@
 export default featureConfig => ({
   setFeature: (featureName, isEnabled) => {
-    featureConfig[featureName] = (typeof isEnabled === "boolean") && isEnabled;
+    featureConfig[featureName] = (typeof isEnabled === 'boolean') && isEnabled;
   },
   featureIsEnabled: featureName => featureConfig[featureName] === true,
 });
