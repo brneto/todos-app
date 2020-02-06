3f94c9c827441bc1870c53a29d70db0bd264e1a8
Berchris Requiao <brneto@gicloud.com>
2020-02-06 09:52:17 -0300

    Use React concurrent mode

--- src/client/index.js
+++ src/client/index.js
@@ -4,10 +4,14 @@ import Root from './components/Root';
 import * as state from './redux/store';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
-  <Root {...state} />,
+// ReactDOM.render(
+//   <Root {...state} />,
+//   document.getElementById('root')
+// );
+
+ReactDOM.createRoot(
   document.getElementById('root')
-);
+).render(<Root {...state} />);
 
 // Learn more about service workers: http://bit.ly/CRA-PWA
 process.env.NODE_ENV !== 'production'
