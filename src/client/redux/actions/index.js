bf9b4518cd33dea164584d1699e940aee9a36073
Berchris Requiao <35544241+brneto@users.noreply.github.com>
2020-02-17 02:35:18 -0300

    Remove resource actions

--- src/client/redux/actions/index.js
+++ src/client/redux/actions/index.js
@@ -2,4 +2,3 @@ export * as effects from './effects';
 export * as commands from './commands';
 export * as events from './events';
 export * as documents from './documents';
-export * as resources from './resources';
