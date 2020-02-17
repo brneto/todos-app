89a29e9806d2775ca95278ae76ee156dc8d876ae
Berchris Requiao <35544241+brneto@users.noreply.github.com>
2020-02-17 02:45:49 -0300

    Create helpers.js

---
+++ src/client/redux/actions/helpers.js
@@ -0,0 +1,12 @@
+
+import { identity } from 'ramda';
+
+// actionCreator :: type -> (data, filter | undefined) -> {
+//   type: 'ACTION_TYPE',
+//   payload: data,
+//   meta: { type: type | [, filter: filter] }
+// }
+const
+  createActionFunction = type => [identity, (payload, filter) => filter ? ({ type, filter }) : ({ type })];
+
+export { createActionFunction };
