f55e9e5bd8d54f01ea6c364bd5eb8a9f3e48022f
Berchris Requiao <35544241+brneto@users.noreply.github.com>
2020-03-13 19:20:29 +0000

    Add comment to replace history

--- src/client/redux/store/index.js
+++ src/client/redux/store/index.js
@@ -1,6 +1,7 @@
 import { createBrowserHistory } from 'history';
 import configStore from './config-store';
 
+// https://github.com/ReactTraining/react-router/blob/master/packages/react-router/docs/api/hooks.md
 const
   history = createBrowserHistory(),
   store = configStore(history);
