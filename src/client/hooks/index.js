8aab512faf3e0f429b155d36c97b0fa584495d0f
Berchris Requiao <brneto@gicloud.com>
2020-03-15 21:43:21 +0000

    Convert LoadingDots to TypeScript

--- src/client/hooks/index.js
+++
@@ -1,3 +0,0 @@
-export * as todos from './todos';
-export * as sse from './sse';
-export * as timer from './timer';
---
+++ src/client/hooks/index.ts
@@ -0,0 +1,6 @@
+// ToDo: https://www.typescriptlang.org/docs/handbook/modules.html#export-all-as-x
+import * as todos from './todos';
+import * as sse from './sse';
+import * as timer from './timer';
+
+export { todos, sse, timer };
