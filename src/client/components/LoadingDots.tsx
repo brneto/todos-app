50772a947fbf5d134e35a77f1281ab8c1e905b52
Berchris Requiao <brneto@gicloud.com>
2020-03-15 23:04:35 +0000

    Downcast children to string

--- src/client/components/LoadingDots.tsx
+++ src/client/components/LoadingDots.tsx
@@ -4,13 +4,12 @@ import * as hooks from '../hooks';
 type loadingDotsProps = {
   delay?: number;
   length?: number;
-  children: string;
 }
 
 const LoadingDots: React.FC<loadingDotsProps> = ({ delay = 300, length = 3, children }) => {
   const count = hooks.timer.useCounterUp(delay) % (length + 1); // Custom Hook
 
-  return <span>{children + '.'.repeat(count)}&nbsp;</span>;
+  return <span>{children as string + '.'.repeat(count)}&nbsp;</span>;
 };
 
 export default LoadingDots;
