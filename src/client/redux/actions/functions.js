c815eba795d983d8c06598ad792b760ff15fc6d5
Berchris Requiao <brneto@gicloud.com>
2020-03-15 17:32:57 +0000

    Update actionFunctions comment

--- src/client/redux/actions/functions.js
+++ src/client/redux/actions/functions.js
@@ -1,10 +1,11 @@
 import { identity } from 'ramda';
 
 // actionCreator :: type -> (data, filter | undefined) -> {
-//   type: 'ACTION_TYPE',
+//   type: 'COMMAND' | 'DOCUMENT' | 'EVENT' | 'SIDE_EFFECT',
 //   payload: data,
 //   meta: { type: type | [, filter: filter] }
 // }
+
 const
   createActionFunction = type =>
     [identity, (payload, filter) => filter ? ({ type, filter }) : ({ type })];
