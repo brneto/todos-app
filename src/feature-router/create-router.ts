65b9cb882c7bc9f898fe89b592c58f1f401c5335
Berchris Requiao <35544241+brneto@users.noreply.github.com>
2020-03-16 22:11:48 +0000

    Remove comment

--- src/feature-router/create-router.ts
+++ src/feature-router/create-router.ts
@@ -4,12 +4,6 @@ type FeatureMap = {
 
 type FeatureKey = keyof FeatureMap;
 
-// ToDo: https://basarat.gitbook.io/typescript/type-system/functions
-// type FeatureRouter = {
-//   setFeature(featureName: keyof FeatureMap, isEnabled: boolean): void;
-//   featureIsEnabled(featureName: keyof FeatureMap): boolean;
-// }
-
 export default (featureConfig: FeatureMap) => ({
   setFeature: (featureName: FeatureKey, isEnable: boolean): void => {
     featureConfig[featureName] = typeof isEnabled === 'boolean' && isEnabled;
