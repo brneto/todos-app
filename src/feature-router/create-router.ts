b574a8e5c51cc1771276f111e93807cf18276fb3
Berchris Requiao <berchris.requiao@ericsson.com>
2020-03-16 16:01:07 +0000

    Update create-router type

--- src/feature-router/create-router.ts
+++ src/feature-router/create-router.ts
@@ -1,14 +1,14 @@
-type featureMap = {
+type FeatureMap = {
   'use-new-feature'?: boolean;
 }
 
 // ToDo: https://basarat.gitbook.io/typescript/type-system/functions
 interface FeatureRouter {
-  setFeature(featureName: keyof featureMap, isEnabled: boolean): void;
-  featureIsEnabled(featureName: keyof featureMap): boolean;
+  setFeature(featureName: keyof FeatureMap, isEnabled: boolean): void;
+  featureIsEnabled(featureName: keyof FeatureMap): boolean;
 }
 
-export default (featureConfig: featureMap): FeatureRouter => ({
+export default (featureConfig: FeatureMap): FeatureRouter => ({
   setFeature: (featureName, isEnabled): void => {
     featureConfig[featureName] = typeof isEnabled === 'boolean' && isEnabled;
   },
