04117b01ae938f4210d2f93932b0cd8342b5721b
Berchris Requiao <berchris.requiao@ericsson.com>
2020-03-22 03:40:57 +0000

    Disable eslint rule to TSX files

--- src/client/components/LoadingDots.tsx
+++ src/client/components/LoadingDots.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+// eslint-disable-next-line import/named
 import { timer } from '../hooks';
 
 type LoadingDotsProps = {
--- src/client/hooks/index.ts
+++ src/client/hooks/index.ts
@@ -1,7 +1,3 @@
-// ToDo: https://www.typescriptlang.org/docs/handbook/modules.html#export-all-as-x
-import * as todos from './todos';
-import * as sse from './sse';
-import * as timer from './timer';
-
-export { todos, sse, timer };
-
+export * as todos from './todos';
+export * as sse from './sse';
+export * as timer from './timer';
