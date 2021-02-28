750452cee7aa2e90d85efbf188dba2d72fc1994b
Berchris Requiao <brneto@gmail.com>
2021-02-28 18:07:51 +0000

    Add docker image

--- src/index.ts
+++
@@ -1,7 +0,0 @@
-import chalk from 'chalk';
-import build from './webpack/builder';
-
-const target = process.env.NODE_ENV ?? 'development';
-
-console.log(chalk.green('Starting app in', target, 'mode...'));
-build(target);
