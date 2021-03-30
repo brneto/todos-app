168bfaad4ed38138c85da53173fe82794770626f
Berchris Requiao <35544241+brneto@users.noreply.github.com>
2021-03-31 00:01:42 +0100

    Update index.ts

--- src/feature-router/index.ts
+++ src/feature-router/index.ts
@@ -1,5 +1,8 @@
 import featureConfig from './feature-config';
 import createFeatureRouter from './create-router';
 
+// Feature Toggles (often also refered to as Feature Flags)
+// are a powerful technique, allowing teams to modify system
+// behavior without changing code.
 export const { setFeature, isFeatureEnabled } = createFeatureRouter(featureConfig);
 
